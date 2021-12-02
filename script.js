8b4e5606b670dbcfdd2388a9a7b1bcad5ec20a21
Bhaskerupadhyay <86480907+Bhaskerupadhyay@users.noreply.github.com>
2021-12-02 13:16:20 +0530

    First page created

---
+++ script.js
@@ -0,0 +1,76 @@
+// const destination = ["unvisited-Gokarna-karnataka", "unvisited-Guzez-valley-kashmir", "unvisited-kanatal-uttarakhand",
+//                         "unvisited-sandakphu-Darjeeling","unvisited-Tada-falls-Andhra-pradesh","unvisited-Arch-Bridge-kerala"];
+const destination = ["ourvission-1", "ourvission-2", "ourvission-1",
+                        "ourvission-2","ourvission-1","ourvission-2"];
+
+let i=0;
+function changedestinationnext(){
+    var banner=document.getElementById("slide-photo");
+    if(i<destination.length-1){
+      i=i+1;
+    }
+    else{
+      i=0;
+    }
+    console.log(destination[i]);
+    document.getElementById("slide-photo").src="./images/"+destination[i]+".jpg";
+    banner.classList.add("zoom");
+    setTimeout(function(){
+      banner.classList.remove("zoom");
+    },500);
+
+}
+function changedestinationprev(){
+  var banner=document.getElementById("slide-photo");
+    if(i>0){
+      i=i-1;
+    }
+    else{
+      i=destination.length-1;
+    }
+    console.log(destination[i]);
+    document.getElementById("slide-photo").src="./images/"+destination[i]+".jpg";
+    banner.classList.add("zoom");
+    setTimeout(function(){
+      banner.classList.remove("zoom");
+    },500);
+
+}
+
+
+
+// our-vission
+
+var btn=document.getElementsByClassName("btn_dot");
+var banner=document.getElementById("banner");
+
+btn[0].onclick=function(){
+   banner.src="images/ourvission-1.jpg";
+   animation_our_vision();
+
+   this.classList.add("btn-dot-col");
+}
+
+btn[1].onclick=function(){
+  banner.src="images/ourvission-2.jpg";
+
+  animation_our_vision();
+  this.classList.add("btn-dot-col");
+}
+btn[2].onclick=function(){
+  banner.src="images/main-photo.jpeg";
+  animation_our_vision();
+
+  this.classList.add("btn-dot-col");
+}
+
+function animation_our_vision(){
+  banner.classList.add("zoom");
+  setTimeout(function(){
+    banner.classList.remove("zoom");
+  },500);
+  for(b of btn){
+    b.classList.remove("btn-dot-col");
+  }
+
+}
